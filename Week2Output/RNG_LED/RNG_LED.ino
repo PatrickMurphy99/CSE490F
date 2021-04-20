5e60c4ada7b9c1675b76ad025b99211448ab2fda
PatrickMurphy99 <71674198+PatrickMurphy99@users.noreply.github.com>
2021-04-20 13:02:44 -0700

    Delete Week2Output directory

--- Week2Output/RNG_LED/RNG_LED.ino
+++
@@ -1,9 +0,0 @@
-#include "BlinkBeat.ino"
-void setup() {
-  BlinkBeat test = new BlinkBeat(4, false, 1000, 1000, HIGH, true);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  test.checkStatus();
-}
