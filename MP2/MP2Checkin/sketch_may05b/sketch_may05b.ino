5b5e0a3f013fb5363f9442f31d0934bfd8f8b174
murphpat <murphpat@uw.edu>
2021-05-05 23:19:39 -0700

    checkpiont tutorial

---
+++ MP2/MP2Checkin/sketch_may05b/sketch_may05b.ino
@@ -0,0 +1,39 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+uint16_t SCREEN_WIDTH = 128; // OLED display width, in pixels
+uint16_t SCREEN_HEIGHT = 64; // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+void setup() { // simple program to manipulate text
+  Serial.begin(9600);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+}
+
+// simply asks users for where they want coordinates for my test message
+// was trying to allow users to customize the message, but Strings in Arduino are hard
+// they are like this in C/C++, and I would rather focus on the display
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("enter a value for x and then for y");
+  Serial.setTimeout(10000);
+  uint16_t x = Serial.parseInt();
+  uint16_t y = Serial.parseInt();
+  String message = "Testing for Pat";
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(x, y);
+  display.println(message);
+  display.display();
+  delay(1000);
+  
+
+}
